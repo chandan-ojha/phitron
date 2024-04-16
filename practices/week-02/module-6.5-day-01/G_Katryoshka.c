f20532da46ae2a51f6001c542355cee4e14f0128
Chandan Ojha <chandanojha776@gmail.com>
2024-04-16 15:55:27 +0600

    #wip

---
+++ practices/week-02/module-6.5-day-01/G_Katryoshka.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int n, m, k;
+    scanf("%lld %lld %lld", &n, &m, &k);
+
+    long long int a = n - m;
+    long long int b = k - m;
+    long int c = a / 2;
+}
