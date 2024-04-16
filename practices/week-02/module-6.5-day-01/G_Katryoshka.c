78d1057473985cba85a1f4a8a2af2b103d986319
Chandan Ojha <chandanojha776@gmail.com>
2024-04-16 22:16:15 +0600

    #G_Katryoshka

--- practices/week-02/module-6.5-day-01/G_Katryoshka.c
+++ practices/week-02/module-6.5-day-01/G_Katryoshka.c
@@ -2,10 +2,35 @@
 
 int main()
 {
-    long long int n, m, k;
-    scanf("%lld %lld %lld", &n, &m, &k);
+    long long int eyes, mouth, body;
 
-    long long int a = n - m;
-    long long int b = k - m;
-    long int c = a / 2;
+    scanf("%lld %lld %lld", &eyes, &mouth, &body);
+
+    long long int min = 0;
+
+    if (eyes < mouth && eyes < body)
+    {
+        min = eyes;
+    }
+    else if (mouth < eyes && mouth < body)
+    {
+        min = mouth;
+    }
+    else
+    {
+        min = body;
+    }
+
+    long long int newEyes = (eyes - min) / 2;
+    long long int newMouth = mouth - min;
+    long long int newBody = body - min;
+
+    if (newEyes <= newBody)
+    {
+        printf("%lld\n", min + newEyes);
+    }
+    else
+    {
+        printf("%lld\n", min + newBody);
+    }
 }
