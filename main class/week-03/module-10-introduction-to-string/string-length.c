13c26999208918e60c3a1b5d6ad1bba708893c44
Chandan Ojha <chandanojha776@gmail.com>
2024-04-22 12:41:16 +0600

    #string-length

---
+++ main class/week-03/module-10-introduction-to-string/string-length.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    /*char a[100];
+    scanf("%s", a);
+
+    int count = 0;
+    int i = 0;
+
+    while (a[i] != '\0')
+    {
+        count++;
+        i++;
+    }
+    printf("%d", count);*/
+
+    char a[100];
+    scanf("%s", a);
+    int st = strlen(a);
+    printf("%d", st);
+
+    return 0;
+}
