c70cb492db6db6ccf08beff7ee3b1f71e1ffcebc
Chandan Ojha <chandanojha776@gmail.com>
2024-04-22 10:57:15 +0600

    #string-initialize-and-print

---
+++ main class/week-03/module-10-introduction-to-string/string-initialize-and-print.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    // Step-01
+    /*char fn[] = {'C', 'h', 'a', 'n', 'd', 'a', 'n'};
+
+    int fnz = sizeof(fn) / sizeof(char);
+
+    for (int i = 0; i < fnz; i++)
+    {
+        printf("%c", fn[i]);
+    }*/
+
+    // Step-02
+    /*char fn[8] = "Chandan\0";
+    printf("%s", fn);*/
+
+    // Step-03
+    char fn[8] = "Chandan";
+    printf("%s", fn);
+
+    return 0;
+}
