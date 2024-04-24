a48b4ce751f749b6d90c1f75307766e3ab60f045
Chandan Ojha <chandanojha776@gmail.com>
2024-04-24 11:09:30 +0600

    #string-lexicographical-comparison

---
+++ main-class/week-03/module-11-string-operation-and-frequency-array/string-lexicographical-comparison.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+
+    /*manual*/
+
+    /*int i = 0;
+
+    while (1)
+    {
+        if (a[i] == '\0' && b[i] == '\0')
+        {
+            printf("Same\n");
+            break;
+        }
+        else if (a[i] == '\0')
+        {
+            printf("A is smaller\n");
+            break;
+        }
+        else if (b[i] == '\0')
+        {
+            printf("B is smaller\n");
+            break;
+        }
+
+        if (a[i] == b[i])
+        {
+            i++;
+        }
+        else if (a[i] < b[i])
+        {
+            printf("A is smaller\n");
+            break;
+        }
+        else
+        {
+            printf("B is smaller\n");
+            break;
+        }
+    }*/
+
+    /*built-in*/
+
+    int v = strcmp(a, b);
+
+    printf("%d\n", v);
+
+    if (v < 0)
+    {
+        printf("A is smaller\n");
+    }
+    else if (v > 0)
+    {
+        printf("B is smaller\n");
+    }
+    else
+    {
+        printf("Same\n");
+    }
+
+    return 0;
+}
