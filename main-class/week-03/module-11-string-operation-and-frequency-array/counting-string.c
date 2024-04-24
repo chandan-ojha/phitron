ed916e2a6e10f91de45f73bacc12d99e5b1fc84a
Chandan Ojha <chandanojha776@gmail.com>
2024-04-24 15:40:13 +0600

    #counting-string

---
+++ main-class/week-03/module-11-string-operation-and-frequency-array/counting-string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[100];
+    scanf("%s", s);
+
+    int count[26] = {0};
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        int value = s[i] - 'a';
+        count[value]++;
+    }
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        int value = s[i] - 'a';
+
+        if (count[value] != 0)
+        {
+            printf("%c count times - %d\n", value + 'a', count[value]);
+        }
+
+        count[value] = 0;
+    }
+
+    return 0;
+}
