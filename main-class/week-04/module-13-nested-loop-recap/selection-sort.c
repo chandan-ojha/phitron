b290e09d5934bda2c0b61d8e1ce38aeb5d880e9f
Chandan Ojha <chandanojha776@gmail.com>
2024-04-28 16:45:20 +0600

    #selection-sort

---
+++ main-class/week-04/module-13-nested-loop-recap/selection-sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // ascending sort
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // printf("%d %d\n", arr[i], arr[j]);
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Ascending Order\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n\n");
+
+    printf("Descending Order\n");
+
+    // descending sort
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // printf("%d %d\n", arr[i], arr[j]);
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
