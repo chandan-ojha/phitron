f28dd6cb1d3bf369d9e18a1800cec8d50c7d4ec4
Chandan Ojha <chandanojha776@gmail.com>
2024-04-29 10:45:19 +0600

    updated

--- main-class/week-04/module-14-function-and-pointer/function_4.c
+++ main-class/week-04/module-14-function-and-pointer/function_4.c
@@ -5,11 +5,12 @@ void sum(void)
     int x, y;
     scanf("%d %d", &x, &y);
     int result = x + y;
-    printf("%d", result);
+    printf("%d\n", result);
 }
 
 int main()
 {
+    sum();
     sum();
     return 0;
 }
