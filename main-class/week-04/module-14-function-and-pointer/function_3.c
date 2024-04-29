3aebe2379c84f2b10d8f43daf5b157db39aaa15b
Chandan Ojha <chandanojha776@gmail.com>
2024-04-29 10:31:42 +0600

    #no-return+parameter

---
+++ main-class/week-04/module-14-function-and-pointer/function_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sum(int x, int y)
+{
+    int result = x + y;
+    printf("%d", result);
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum(a, b);
+
+    return 0;
+}
