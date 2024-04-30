a2e68e8dbed7c3d69c020e55743b90519f7072d4
Chandan Ojha <chandanojha776@gmail.com>
2024-04-30 23:34:21 +0600

    #A_Three_Doors

--- conceptual-session/week-04/session-01-function/A_Three_Doors.c
+++ conceptual-session/week-04/session-01-function/A_Three_Doors.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+
+void doors_unlock()
+{
+    int key;
+    scanf("%d", &key);
+    int arr[4];
+
+    for (int i = 1; i <= 3; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    if (arr[key] == 0)
+    {
+        printf("NO\n");
+        return;
+    }
+
+    key = arr[key];
+
+    if (arr[key] == 0)
+    {
+        printf("NO\n");
+        return;
+    }
+
+    printf("YES\n");
+}
+
 int main()
 {
+    int test;
+    scanf("%d", &test);
+
+    while (test--)
+    {
+        doors_unlock();
+    }
 
     return 0;
 }
