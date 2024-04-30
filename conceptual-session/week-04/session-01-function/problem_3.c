1445c08aa2c3e718037dbdd8e924b5151a3d2a8c
Chandan Ojha <chandanojha776@gmail.com>
2024-04-30 17:42:46 +0600

    #problem_3 solved

---
+++ conceptual-session/week-04/session-01-function/problem_3.c
@@ -0,0 +1,18 @@
+/***
+ * Example : Phi value
+ *
+ */
+
+#include <stdio.h>
+
+float getPHI(void)
+{
+    return 3.1416;
+}
+
+int main()
+{
+    float PI = getPHI();
+    printf("%0.4f", PI);
+    return 0;
+}
