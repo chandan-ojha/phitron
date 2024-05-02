271c32f6e688ac4ce9f3b1751e594ef9dbbb1471
Chandan Ojha <chandanojha776@gmail.com>
2024-05-02 16:05:08 +0600

    #function_and_array_as_reference

---
+++ main-class/week-04/module-15-function-with-pointer/function_and_array_as_reference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fun(int *arr, int n)
+{
+    arr[1] = 400;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+
+    fun(arr, 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
