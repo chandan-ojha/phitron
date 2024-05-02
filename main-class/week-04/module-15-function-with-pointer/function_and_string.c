2b48bcd800c63dadba6421f27c38679d47e5e386
Chandan Ojha <chandanojha776@gmail.com>
2024-05-02 15:50:14 +0600

    #function_and_string

--- main-class/week-04/module-15-function-with-pointer/function_and_string.c
+++ main-class/week-04/module-15-function-with-pointer/function_and_string.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void fun(char arr[])
+{
+    // int sz = sizeof(arr) / sizeof(char);
+    // printf("%d", sz);
+
+    printf("%d\n", strlen(arr));
+
+    // char n[] = "World";
+    // return n;
+}
+
 int main()
 {
+    char arr[20] = "Hello";
+
+    // int sz = sizeof(arr) / sizeof(char);
+    // printf("%d", sz);
+
+    fun(arr);
 
     return 0;
 }
