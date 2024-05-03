2e769398b295ab8586775010e7ed93224d683e7b
Chandan Ojha <chandanojha776@gmail.com>
2024-05-03 13:00:50 +0600

    #Pattern solved

---
+++ assignments/assignment-03/Pattern.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int s = n - 1;
+    int c = 1;
+
+    for (int i = 1; i <= (2 * n) - 1; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= c; j++)
+        {
+            if (i % 2 != 0)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf("-");
+            }
+        }
+
+        if (i < n)
+        {
+            s--;
+            c = c + 2;
+        }
+        else
+        {
+            s++;
+            c = c - 2;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
