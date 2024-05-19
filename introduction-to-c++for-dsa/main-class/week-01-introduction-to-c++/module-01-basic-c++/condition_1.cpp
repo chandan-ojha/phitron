aedadf9fec0dcb9543e13f11a1f1beaf35ce7c99
Chandan Ojha <chandanojha776@gmail.com>
2024-05-19 12:47:09 +0600

    #if-else

---
+++ introduction-to-c++for-dsa/main-class/week-01-introduction-to-c++/module-01-basic-c++/condition_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 11;
+
+    if (x == 10)
+    {
+        cout << "The number is ten\n";
+    }
+    else
+    {
+        cout << "The number is not ten" << endl;
+    }
+    return 0;
+}
