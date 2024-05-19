de9639c328ca53ea1f3c5eb2e6533de77219197e
Chandan Ojha <chandanojha776@gmail.com>
2024-05-19 10:34:34 +0600

    #start-basic-c++

---
+++ introduction-to-c++for-dsa/main-class/week-01-introduction-to-c++/module-01-basic-c++/output.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+    // std::cout << "Hello World";
+
+    int a = 5;
+    long long int b = 100000000000000;
+    float c = 1.5;
+    double d = 1.5666666;
+    char ch = 'C';
+
+    // std::cout << a;
+
+    // std::cout << "My favourite number is " << a;
+
+    // std::cout << 10 << " is my favourite number";
+
+    // std::cout << "Number " << a << " is my favourite!";
+
+    // std::cout << a << " " << b;
+
+    // std::cout << a << "\n" << b << "\n";
+
+    std::cout << a << std::endl
+              << b << std::endl;
+
+    return 0;
+}
