09432bbd5cc002f77c881c022555ab7167bd3bae
Chandan Ojha <chandanojha776@gmail.com>
2024-05-19 14:01:50 +0600

    #switch-case

---
+++ introduction-to-c++for-dsa/main-class/week-01-introduction-to-c++/module-01-basic-c++/switch_case_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int x;
+
+    cin >> x;
+
+    switch (x)
+    {
+    case 1:
+        cout << "One" << endl;
+        break;
+    case 2:
+        cout << "two" << endl;
+        break;
+    case 3:
+        cout << "three" << endl;
+        break;
+    case 4:
+        cout << "four" << endl;
+        break;
+    case 5:
+        cout << "five" << endl;
+        break;
+    default:
+        cout << "No match" << endl;
+    }
+
+    return 0;
+}
