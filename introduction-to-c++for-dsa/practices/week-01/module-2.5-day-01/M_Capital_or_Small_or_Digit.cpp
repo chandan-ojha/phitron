1bd7f4528602f5438c5d2e28b71bafc2f66b46e7
Chandan Ojha <chandanojha776@gmail.com>
2024-05-21 10:02:53 +0600

    Update M_Capital_or_Small_or_Digit.cpp

--- introduction-to-c++for-dsa/practices/week-01/module-2.5-day-01/M_Capital_or_Small_or_Digit.cpp
+++ introduction-to-c++for-dsa/practices/week-01/module-2.5-day-01/M_Capital_or_Small_or_Digit.cpp
@@ -2,4 +2,24 @@
 using namespace std;
 int main()
 {
+    char x;
+    cin >> x;
+
+    if (x >= '0' && x <= '9')
+    {
+        cout << "IS DIGIT" << endl;
+    }
+    else
+    {
+        cout << "ALPHA" << endl;
+
+        if (x >= 'A' && x <= 'Z')
+        {
+            cout << "IS CAPITAL" << endl;
+        }
+        else
+        {
+            cout << "IS SMALL" << endl;
+        }
+    }
 }
