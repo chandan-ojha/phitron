8b3de138755bad6fae975defe2e375bcf8131eec
Chandan Ojha <chandanojha776@gmail.com>
2024-05-22 15:27:24 +0600

    Create constructor.cpp

---
+++ introduction-to-c++for-dsa/main-class/week-01-introduction-to-c++/module-03-class-and-object/constructor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+    Student(int roll, int cls, double gpa)
+    {
+        // (*this).roll = roll;
+        // (*this).cls = cls;
+        // (*this).gpa = gpa;
+
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+int main()
+{
+    Student chandan(1, 10, 4.86);
+    cout << chandan.roll << " " << chandan.cls << " " << chandan.gpa << endl;
+
+    return 0;
+}
