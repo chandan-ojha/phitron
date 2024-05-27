3943a2ab2f950b728388d92175f5ac32b58c8f5c
Chandan Ojha <chandanojha776@gmail.com>
2024-05-27 11:57:08 +0600

    Create string_constructor.cpp

---
+++ introduction-to-c++for-dsa/main-class/week-02-more-about-class-and-string/module-06-more-about-class-and-string/string_constructor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "hello world";
+
+    /*1st option*/
+    // string s("hello world");
+
+    /*2nd option*/
+    // string s("hello world", 7);
+
+    /*3rd option*/
+    // string a = "hello world";
+    // string s(a, 4);
+
+    /*4th option*/
+    string s(26, 'a');
+    cout << s << endl;
+
+    return 0;
+}
