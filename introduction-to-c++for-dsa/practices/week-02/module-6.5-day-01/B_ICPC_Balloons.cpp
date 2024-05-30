0c793eab2ac832031818a764f97d966f38bd7b99
Chandan Ojha <chandanojha776@gmail.com>
2024-05-30 16:53:38 +0600

    Update B_ICPC_Balloons.cpp

--- introduction-to-c++for-dsa/practices/week-02/module-6.5-day-01/B_ICPC_Balloons.cpp
+++ introduction-to-c++for-dsa/practices/week-02/module-6.5-day-01/B_ICPC_Balloons.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 int main()
 {
-    // TODO
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        char s[n];
+        cin >> s;
+
+        int count[26] = {0};
+        int sum = 0;
+
+        for (char c : s)
+        {
+            if (count[c - 'A'] == 0)
+            {
+                sum = sum + 2;
+                count[c - 'A']++;
+            }
+            else
+            {
+                sum = sum + 1;
+                count[c - 'A']++;
+            }
+        }
+
+        cout << sum << endl;
+    }
+
     return 0;
 }
