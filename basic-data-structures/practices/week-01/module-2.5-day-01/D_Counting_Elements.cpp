1c5ab8f38e76d85e816090ac450bef98d95d97b3
Chandan Ojha <chandanojha776@gmail.com>
2024-06-11 23:33:21 +0600

    Create D_Counting_Elements.cpp

---
+++ basic-data-structures/practices/week-01/module-2.5-day-01/D_Counting_Elements.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = a[i] + 1;
+
+        auto it = find(a.begin(), a.end(), sum);
+
+        if (it != a.end())
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
