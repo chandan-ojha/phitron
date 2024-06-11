4ffd36c940ce0be8d6ffacc06cde93b6a7133bfd
Chandan Ojha <chandanojha776@gmail.com>
2024-06-11 11:26:08 +0600

    #problem-2-solved

---
+++ basic-data-structures/main-class/week-01-introduction-to-data-structure/module-02-stl-vector/practice-problem/problem_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(5, 7);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
