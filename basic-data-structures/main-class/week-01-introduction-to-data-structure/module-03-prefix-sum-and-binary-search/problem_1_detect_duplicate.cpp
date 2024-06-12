31d01dffa81c5d617b76df0e0bb463e9336f872e
Chandan Ojha <chandanojha776@gmail.com>
2024-06-12 17:40:54 +0600

    #wip

---
+++ basic-data-structures/main-class/week-01-introduction-to-data-structure/module-03-prefix-sum-and-binary-search/problem_1_detect_duplicate.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int x;
+    cin >> x;
+    int l = 0;
+    int r = n - 1;
+    int duplicate = 0;
+
+    while (l <= r)
+    {
+        int mid_index = (l + r) / 2;
+
+        if (a[mid_index] == x)
+        {
+            duplicate++;
+        }
+
+        if (x > a[mid_index])
+        {
+            l = mid_index + 1;
+        }
+        else
+        {
+            r = mid_index - 1;
+        }
+    }
+
+    if (duplicate >= 1)
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
