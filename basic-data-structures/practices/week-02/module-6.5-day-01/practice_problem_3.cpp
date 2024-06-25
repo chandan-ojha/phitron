fdf9645538b9d5d4e14429a6b8ea523c15174c5a
Chandan Ojha <chandanojha776@gmail.com>
2024-06-25 21:44:40 +0600

    #problem-3-solved

---
+++ basic-data-structures/practices/week-02/module-6.5-day-01/practice_problem_3.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void take_input(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+}
+
+void print_middle_element(Node *head)
+{
+    Node *tmp = head;
+
+    int count = 0;
+
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+
+    tmp = head;
+
+    if (count % 2 != 0)
+    {
+        int mid_el = count / 2;
+
+        for (int i = 0; i < mid_el; i++)
+        {
+            tmp = tmp->next;
+        }
+
+        cout << tmp->val << endl;
+    }
+    else
+    {
+        int mid_el = count / 2 - 1;
+
+        for (int i = 0; i < mid_el; i++)
+        {
+            tmp = tmp->next;
+        }
+
+        cout << tmp->val << " " << tmp->next->val << endl;
+    }
+}
+
+int main()
+{
+    int val;
+    Node *head = NULL;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        take_input(head, val);
+    }
+
+    print_middle_element(head);
+
+    return 0;
+}
