03020db096bfe82639b6f34363617416ae390441
Chandan Ojha <chandanojha776@gmail.com>
2024-06-27 11:04:09 +0600

    #practice-problem-1-solved

---
+++ basic-data-structures/practices/week-02/module-7.5-day-02/practice_problem_1.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+int main()
+{
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+
+    int val;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_tail(head1, tail1, val);
+    }
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_tail(head2, tail2, val);
+    }
+
+    int size1 = size(head1);
+    int size2 = size(head2);
+
+    if (size1 == size2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
