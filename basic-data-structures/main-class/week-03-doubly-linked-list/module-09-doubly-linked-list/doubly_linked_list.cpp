e291e02acbcb5d2bb9798438c87855a906830932
Chandan Ojha <chandanojha776@gmail.com>
2024-06-29 22:26:53 +0600

    #doubly_linked_list

---
+++ basic-data-structures/main-class/week-03-doubly-linked-list/module-09-doubly-linked-list/doubly_linked_list.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void print_left_to_right(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void print_right_to_left(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = b;
+
+    // connection
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+
+    print_left_to_right(head);
+    print_right_to_left(tail);
+
+    return 0;
+}
