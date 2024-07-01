44b99c3d34f4db89ec8c453eb8d0677046b17df9
Chandan Ojha <chandanojha776@gmail.com>
2024-07-01 11:54:29 +0600

    #list-operations

---
+++ basic-data-structures/main-class/week-03-doubly-linked-list/module-10-stl-list-and-cycle-detection/operations_functions.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> myList = {20, 40, 30, 10, 50, 10, 10};
+
+    // myList.remove(10);
+    // sort(myList.begin(), myList.end());
+
+    // myList.sort(); // time complexity O(NlogN)
+    // myList.sort(greater<int>());
+    // myList.unique();
+    myList.reverse(); // time complexity O(N)
+
+    for (int val : myList)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
