70a2eb36516ffd84035ef44893c6277d7f8584c7
Chandan Ojha <chandanojha776@gmail.com>
2024-07-09 18:20:10 +0600

    #problem-3-solved

---
+++ basic-data-structures/practices/week-04/module-14.5-day-01/practice_problem_3.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> s1, s2;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        s1.push(val);
+    }
+
+    while (!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+
+    while (!s2.empty())
+    {
+        cout << s2.top() << " ";
+        s2.pop();
+    }
+
+    return 0;
+}
